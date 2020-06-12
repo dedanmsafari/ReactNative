426fabeb7dbecb985c31efefd1eef9ee789e884d
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-12 17:58:21 +0300

    Our Picker Component

---
+++ app/components/Picker.js
@@ -0,0 +1,65 @@
+import React,{useState} from 'react'
+import { StyleSheet,Button, Text, View, TextInput,Platform, TouchableWithoutFeedback, Modal, FlatList } from 'react-native'
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+// import colors from '../config/colors'
+import defaultStyles from '../config/styles';
+import AppText from './Text';
+import Screen from './Screen';
+import PickerItem from './PickerItem';
+const AppPicker = ({icon,items,numberOfColumns = 1 ,onSelectItem,PickerItemComponent = PickerItem ,width='100%', placeholder, selectedItem}) => {
+    const [modalVisible, setModalVisible] = useState(false)
+    
+    return (
+        <>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+
+        <View style={[styles.container, { width }]}>
+{icon && <MaterialCommunityIcons name={icon} size={30} color={defaultStyles.colors.grey}  />}
+    {selectedItem ? (<AppText style={styles.text}>{selectedItem.label}</AppText>) : (<AppText style={styles.placeholder}>{placeholder}</AppText>)}
+    <MaterialCommunityIcons name='chevron-down' size={30} color={defaultStyles.colors.grey}  style={styles.icon} />
+        </View>
+
+        </TouchableWithoutFeedback>
+        <Modal visible={modalVisible} animationType='slide'>
+<Screen>
+<Button title='Close' onPress={() => setModalVisible(false)}/>
+<FlatList 
+data={items}
+keyExtractor={ item => item.value.toString()}
+numColumns={numberOfColumns}
+renderItem={({item}) => <PickerItemComponent item={item}  onPress={() =>{ 
+  setModalVisible(false);
+  onSelectItem(item)
+}}/>}
+/>
+</Screen>
+        </Modal>
+        </>
+    )
+} 
+
+export default AppPicker
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:defaultStyles.colors.medium,
+        borderRadius:25,
+        flexDirection:'row',
+  
+        padding:15,
+        marginVertical: 10
+    },
+    icon:{
+ marginRight: 3,
+    },
+    placeholder:{
+        color:defaultStyles.colors.light,
+        flex:1
+    },
+
+    text:{
+        flex:1,
+        padding:3
+    }
+ 
+})
