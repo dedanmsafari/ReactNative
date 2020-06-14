193365ed81db6027d4193d95f379087fea5cd58e
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-14 08:57:09 +0300

    Form field component

---
+++ app/components/forms/FormField.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { useFormikContext} from 'formik'
+import TextInput from '../TextInput'
+import ErrorMessage from './ErrorMessage'
+
+
+const AppFormField = ({name, width, ...otherProps}) => {
+const { setFieldTouched, handleChange , errors, touched } = useFormikContext();
+
+    return (
+  <>
+
+<TextInput
+onBlur={() => setFieldTouched(name)}
+onChangeText={handleChange(name)}
+width={width}
+{...otherProps}
+/>
+<ErrorMessage error={errors[name]} visible={touched[name]}/>
+
+  </>
+    )
+}
+
+export default AppFormField
+
+const styles = StyleSheet.create({})
