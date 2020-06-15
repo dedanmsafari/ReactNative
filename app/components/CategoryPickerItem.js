a65bcedf3c75963593d147110a3aab2c40098e0e
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-15 07:12:11 +0300

    Category picker Item

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import AppText from './Text'
 import Icon from './Icon'
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+
 const CategoryPickerItem = ({ item,onPress}) => {
     return   (
-  
-        <View style={styles.container} >
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+
+        <View  >
  <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
     <AppText style={styles.label}>{item.label}</AppText>
         </View>
+  </TouchableOpacity>
     )
     
 }
