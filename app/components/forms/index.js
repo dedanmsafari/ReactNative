71b166b6a0150791dd9d06f566b3d3c06bde43e3
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-17 17:40:11 +0300

    index master js

---
+++ app/components/forms/index.js
@@ -0,0 +1,5 @@
+export {default as AppForm } from './Form';
+export {default as AppFormField } from './FormField';
+export {default as AppFormPicker} from  './FormPicker';
+export {default as ErrorMessage } from './ErrorMessage';
+export {default as SubmitButton } from './SubmitButton';
