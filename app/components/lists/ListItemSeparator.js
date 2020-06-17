1c0c64be6daa82f335877c25d4d843c50ec7f5c1
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-17 17:40:56 +0300

    item separators

---
+++ app/components/lists/ListItemSeparator.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View,StyleSheet } from 'react-native'
+import colors from '../../config/colors'
+
+const ListItemSeparator = () => {
+    return (
+        <View style={styles.separator}/>
+    )
+}
+
+export default ListItemSeparator
+
+const styles = StyleSheet.create({
+    separator:{
+        width:'100%',
+        height:1,
+        backgroundColor:colors.secondary
+    }
+})
