45e0ee7ace07c47d271cb4626beb99162740ef5c
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-18 04:21:20 +0300

    colors palette

---
+++ app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+primary:"#EC5578",
+secondary:"#7285A5",
+auxiliary:"#29AB87",
+white:"#FDFFFC",
+medium: '#EDF2F4',
+light:'#ADB5DB',
+lighter:"#ECF8F8",
+dark:"#0C0C0C",
+grey:'#6C757D'
+}
