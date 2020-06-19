a810668fcb28d7e1df2895eb41abdd8ef6014171
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-19 03:34:59 +0300

    Form component

--- app/components/forms/Form.js
+++ app/components/forms/Form.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import {Formik} from 'formik'
+import React from "react";
+import { Formik } from "formik";
 
-const AppForm = ({initialValues,onSubmit,validationSchema,children}) => {
-    return (
-        <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-        >
-{()=>(
-    <>
-{children}
-    </>
-)}
-            </Formik>
-    )
-}
+const AppForm = ({ initialValues, onSubmit, validationSchema, children }) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {() => <>{children}</>}
+    </Formik>
+  );
+};
 
-export default AppForm
-
+export default AppForm;
