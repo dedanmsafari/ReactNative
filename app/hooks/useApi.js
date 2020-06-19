1a3ebc9b6bb1dcf06a2fb0aff35c71ee2e12e394
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-19 03:35:26 +0300

    useApi Component

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,22 +1,20 @@
-import React , { useState} from "react";
+import React, { useState } from "react";
 
 export default useApi = (apiFunc) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const request = async (...args) => {
-        setLoading(true);
-        const response = await apiFunc(...args);
-        setLoading(false);
-    
-        if (!response.ok) return setError(true);
-    
-        setError(false);
-        setData(response.data);
-    };
-    
-    
-    return { data, error, loading , request }
+  const request = async (...args) => {
+    setLoading(true);
+    const response = await apiFunc(...args);
+    setLoading(false);
 
-}
+    if (!response.ok) return setError(true);
+
+    setError(false);
+    setData(response.data);
+  };
+
+  return { data, error, loading, request };
+};
