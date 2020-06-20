910b7efdb027c1e6c1472b7bdb677293678e9e54
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-20 20:54:16 +0300

    Categorypicker component

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import AppText from './Text'
-import Icon from './Icon'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import AppText from "./Text";
+import Icon from "./Icon";
 
-const CategoryPickerItem = ({ item,onPress}) => {
-    return   (
-  <TouchableOpacity style={styles.container} onPress={onPress}>
+const CategoryPickerItem = ({ item, onPress }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+      </TouchableOpacity>
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  );
+};
 
-        <View  >
- <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
-    <AppText style={styles.label}>{item.label}</AppText>
-        </View>
-  </TouchableOpacity>
-    )
-    
-}
-
-export default CategoryPickerItem
+export default CategoryPickerItem;
 
 const styles = StyleSheet.create({
-    container:{
-paddingHorizontal:15,
-paddingVertical:15,
-alignItems:'center',
-width:'33%'
-    },
-    label:{
-        marginTop:6,
-        textAlign:"center"
-    }
-})
+  container: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 6,
+    textAlign: "center",
+  },
+});
