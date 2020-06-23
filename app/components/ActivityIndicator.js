03a68cf7a0b43f7ae8eaa4a2974cdf26d43ffd36
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-23 03:40:30 +0300

    activity indicators

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,14 +1,30 @@
 import React from "react";
 import LottieView from "lottie-react-native";
+import { View, StyleSheet } from "react-native";
 
-const ActivityIndicator = ({ visible = false}) => {
+const ActivityIndicator = ({ visible = false }) => {
   if (!visible) return null;
-  return <LottieView
-      autoPlay
-      loop
-      source={require('../assets/animations/loader.json')}
-    />
-  
+  return (
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require("../assets/animations/loader.json")}
+      />
+    </View>
+  );
 };
 
 export default ActivityIndicator;
+
+const styles = StyleSheet.create({
+
+  overlay:{
+    position:'absolute',
+    backgroundColor:'white',
+    height:'100%',
+    opacity:0.8,
+    width:'100%',
+    zIndex:1
+  }
+});
