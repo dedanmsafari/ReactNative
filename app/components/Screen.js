3b66aa564804fcecb8bd56cb8b4ed3be8d5b85b4
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-24 16:27:19 +0300

    screen component

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import Constants from 'expo-constants';
+import React from "react";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import Constants from "expo-constants";
 
-const Screen = ({children,style}) => {
-    return (
-       <SafeAreaView style={[styles.screen,style]}>
-           <View style={[styles.view,style]}>
+const Screen = ({ children, style }) => {
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={[styles.view, style]}>{children}</View>
+    </SafeAreaView>
+  );
+};
 
-           {children}
-           </View>
-       </SafeAreaView>
-    )
-}
-
-export default Screen
+export default Screen;
 
 const styles = StyleSheet.create({
-    screen:{
-        paddingTop: Constants.statusBarHeight,
-      flex:1
-    },
-    view:{
-        flex: 1
-
-    }
-})
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
+    flex: 1,
+  },
+});
