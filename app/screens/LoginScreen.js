19fa94aa67a27a71f8c2fef0df4df22b57a199fa
Dedan Msafari <dedan.msafari@gmail.com>
2020-06-25 07:25:52 +0300

    Login Screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,73 +1,83 @@
-import React from 'react'
-import { StyleSheet,Image } from 'react-native'
+import React, {useState} from "react";
+import { StyleSheet, Image } from "react-native";
 
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-import Screen from '../components/Screen'
-import {AppForm, AppFormField,SubmitButton} from '../components/forms'
-
+import Screen from "../components/Screen";
+import {
+  Form,
+  FormField,
+  ErrorMessage,
+  SubmitButton,
+} from "../components/forms";
+import authApi from "../api/auth";
+import useAuth from '../auth/useAuth'
 
 const validationSchema = Yup.object().shape({
-    email: Yup.string().required().email().label('Email'),
-    password: Yup.string().required().min(8).label('Password')
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
 });
 
-
-
 const LoginScreen = () => {
+  const { logIn } = useAuth();
+  const [loginFailed, setLoginFailed] = useState(false);
 
-    return (
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
+    if (!result.ok) return setLoginFailed(true);
+
+    logIn(result.data);
+  };
+
+  return (
     <Screen style={styles.container}>
-<Image
-style={styles.logo}
-source={require('../assets/logo-red.png')}/>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
-
-<AppForm
-initialValues={{ email:'', password:'' }}
-onSubmit={values => console.log(values)
-}
-validationSchema={validationSchema}
->
-
-    <AppFormField
-autoCapitalize="none"
-autoCorrect={false}
-icon='email'
-keyboardType='email-address'
-name='email'
-placeholder='Email Address'
-textContentType='emailAddress'
-/>
-<AppFormField
-autoCapitalize='none'
-autoCorrect={false}
-icon='lock'
-name='password'
-placeholder='password'
-secureTextEntry
-textContentType='password'
-/>
-<SubmitButton title='Login'/>
-
-</AppForm>
-
+      <Form
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <ErrorMessage
+          error="Invalid email and/or password"
+          visible={loginFailed}
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Login" />
+      </Form>
     </Screen>
-    )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container:{
-  padding:10
-    },
-    logo:{
-        width: 80,
-        height: 80,
-        alignSelf: "center",
-      
-        marginTop:50,
-        marginBottom:30
-    }
-})
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+
+    marginTop: 50,
+    marginBottom: 30,
+  },
+});
